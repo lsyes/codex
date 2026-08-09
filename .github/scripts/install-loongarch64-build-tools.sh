9cd2f33d269f91e08e9bd101598aa369d47a2aaa
lsyes <3294271542@qq.com>
2026-08-09 10:46:40 +0800

    Add lld to Loongarch64 build tools installation

--- .github/scripts/install-loongarch64-build-tools.sh
+++ .github/scripts/install-loongarch64-build-tools.sh
@@ -20,6 +20,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
   gcc-13-loongarch64-linux-gnu \
   g++-13-loongarch64-linux-gnu \
   binutils-loongarch64-linux-gnu \
+  lld \
   xz-utils
 
 libcap_version="2.75"
